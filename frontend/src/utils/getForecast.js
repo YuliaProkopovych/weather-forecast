ff3131243fb5f3a79df4f50562ab5bd5b6a27fa2
Yulia <schonebud@gmail.com>
2021-11-17 16:26:44 +0200

    Fix styling weather tables

--- frontend/src/utils/getForecast.js
+++ frontend/src/utils/getForecast.js
@@ -2,6 +2,7 @@ import { DateTime } from "luxon";
 
 import { WeatherIcon, WindDirectionIcon } from "../components/icon";
 import { Box, Text } from 'grommet';
+import _ from 'lodash';
 
 const getForecast = async (location) => {
   console.log('getting forecast');
@@ -13,21 +14,44 @@ const getForecast = async (location) => {
   const response = await fetch('http://127.0.0.1:3000/forecast', requestOptions);
 
   const weather = await response.json();
-  const formattedWeather = weather.map(entity => {
 
-    return { time: DateTime.fromISO(entity.time).toFormat('ff'),
-    weather: <WeatherIcon path={ `/icons/svg/${entity.symbol}.svg` }/>,
-    temperature: entity.weather.air_temperature,
-    precipations: '',
-    wind: <Box direction='row' justify='evenly'>
-        <Text margin={{right: '10px'}}>{ entity.weather.wind_speed }</Text>
-        <WindDirectionIcon angle={ 180 + entity.weather.wind_from_direction } />
-      </Box>
+  const formattedWeather = weather.map(entity => {
+    return {
+      date: DateTime.fromISO(entity.time).toFormat('dd LLLL'),
+      time: DateTime.fromISO(entity.time).toFormat('hh:mm'),
+      weather: <WeatherIcon path={ `/icons/svg/${entity.symbol}.svg` }/>,
+      temperature: entity.weather.air_temperature,
+      precipations: '',
+      wind: (
+        <Box direction='row' justify='evenly'>
+          <Text margin={{right: '10px'}}>{ entity.weather.wind_speed }</Text>
+          <WindDirectionIcon angle={ 180 + entity.weather.wind_from_direction } />
+        </Box>
+      )
     }
   });
-  console.log(formattedWeather);
 
-  return formattedWeather;
+  return formattedWeather.reduce((accumulator, forecastItem) => {
+    const lastDay = accumulator[accumulator.length - 1];
+    const { date, ...rest } = forecastItem;
+
+    if (lastDay?.date === forecastItem.date) {
+      lastDay.forecast.push(rest);
+
+      return accumulator;
+    }
+
+    return [
+      ...accumulator,
+      {
+        date,
+        forecast: [
+          rest,
+        ],
+      },
+    ];
+  }, []);
+
 }
 
 export default getForecast;
