264ef0a5cc0e6ecf0584f503f68384bcf62fe0ae
Yulia <schonebud@gmail.com>
2021-11-19 13:24:44 +0200

    Fix precipitation displaying

--- backend/utils/getForecast.js
+++ backend/utils/getForecast.js
@@ -12,7 +12,21 @@ getForecastByCoordinates = async (coordinates) => {
   response.data['properties'].timeseries.map(item => {console.log(item.data)});
 
   const forecast = response.data['properties'].timeseries.map(
-    entity => ({ time: entity.time, weather: entity.data.instant.details, symbol: entity.data.next_1_hours ? entity.data.next_1_hours.summary.symbol_code : entity.data.next_6_hours?.summary.symbol_code })
+    record => {
+      let formattedRecord = { time: record.time,
+        weather: record.data.instant.details
+      };
+
+      if (record.data.next_6_hours) {
+        formattedRecord.next_6_hours = {symbol: record.data.next_6_hours.summary.symbol_code, precipitations: record.data.next_6_hours.details.precipitation_amount}
+      };
+
+      if (record.data.next_1_hours) {
+        formattedRecord.next_1_hours = {symbol: record.data.next_1_hours.summary.symbol_code, precipitations: record.data.next_1_hours.details.precipitation_amount}
+      };
+
+      return formattedRecord;
+    }
   );
 
   return forecast;
