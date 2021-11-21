8991bbd1e576d3806ffef34a656d2fc68d63b3ef
Yulia <schonebud@gmail.com>
2021-11-21 16:42:41 +0200

    Create weather preview by date

--- frontend/src/utils/autocomplete.js
+++ frontend/src/utils/autocomplete.js
@@ -3,7 +3,6 @@ import axios from "axios";
 const locationAutocomplete = async (query) => {
   const results = await axios.get(`https://autocomplete.search.hereapi.com/v1/autocomplete?limit=20&lang=en-US&q=${query}&apiKey=4SIwA2_PMQuFpVFrLJENdTsg6fZjhkgtHjvTu-xa6fc`);
   const locations = results.data.items.filter(place => {
-    //console.log(place.resultType, place?.localityType, place?.administrativeAreaType);
     return (place.resultType === 'locality' && place.localityType === 'city') || (place.resultType === 'locality' && place.localityType === 'district')
   }).map(item => item.title);
   return locations;
