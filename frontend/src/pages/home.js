e1f38717565aeece9825ad2cec73f95e9527ba74
Yulia <schonebud@gmail.com>
2021-12-14 13:06:29 +0200

    Create list of favourite places on home page

--- frontend/src/pages/home.js
+++ frontend/src/pages/home.js
@@ -1,16 +1,28 @@
-import React, { useState } from 'react';
-import { Box } from 'grommet';
+import React, { useState, useEffect } from 'react';
+import { Box, List } from 'grommet';
 
 import SearchForm from '../components/SearchForm';
+import FavouriteLocationsList from '../components/FavouriteLocationsList';
 
 function Home() {
   const [location, setLocation] = useState('');
+  const [favouriteLocations, setFavouriteLocations] = useState([]);
+
+  useEffect(() => {
+    const favLocations = JSON.parse(localStorage.getItem('Favourite Locations'));
+    if (favLocations) {
+      setFavouriteLocations(favLocations);
+    }
+  });
 
   return (
     <Box>
       <SearchForm
         location={location}
       />
+      <Box>
+        {favouriteLocations.length !== 0 && <FavouriteLocationsList locations={favouriteLocations} />}
+      </Box>
     </Box>
   );
 }
