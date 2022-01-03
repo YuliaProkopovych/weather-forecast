3fe3b0fc263287f6b50cda26bd8d110269ccfc33
Yulia <schonebud@gmail.com>
2022-01-03 12:29:39 +0200

    Change background gradient

--- frontend/src/components/background.js
+++ frontend/src/components/background.js
@@ -1,11 +1,15 @@
 import styled from 'styled-components';
 import { Box } from 'grommet';
 
+// const BackgroundBox = styled(Box)`
+// background: radial-gradient(at 0 10%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 21%, rgba(255,255,255,0.3) 31%, rgba(255,255,255,0) 100%),
+// radial-gradient(at 0 40%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 51%, rgba(255,255,255,0.3) 81%, rgba(255,255,255,0) 100%),
+// radial-gradient(at 10% 80%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 51%, rgba(255,255,255,0.3) 81%, rgba(255,255,255,0) 100%),
+// radial-gradient(at 60% 30px, #f7e413 20px, #ebf38b 180px, #adfbda 50%);
+// `;
+
 const BackgroundBox = styled(Box)`
-background: radial-gradient(at 0 10%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 21%, rgba(255,255,255,0.3) 31%, rgba(255,255,255,0) 100%),
-radial-gradient(at 0 40%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 51%, rgba(255,255,255,0.3) 81%, rgba(255,255,255,0) 100%),
-radial-gradient(at 10% 80%, rgba(180,180,180,0.9) 0, rgba(180,180,180,0.8) 11%, rgba(255,255,255,0.6) 51%, rgba(255,255,255,0.3) 81%, rgba(255,255,255,0) 100%),
-radial-gradient(at 60% 30px, #f7e413 20px, #ebf38b 180px, #adfbda 50%);
+background: linear-gradient(180deg, #edf2fb 100px, #e2eafc 150px, #d7e3fc 250px, #ccdbfd 400px, #c1d3fe 600px, #b6ccfe 800px, #abc4ff 1000px);
 `;
 
 export default BackgroundBox;
