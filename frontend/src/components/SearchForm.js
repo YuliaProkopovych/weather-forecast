6c30c538908faaf5e21d56cfd465109d5501d45c
Yulia <schonebud@gmail.com>
2022-02-09 15:38:59 +0200

    Search form validation and styling

--- frontend/src/components/SearchForm.js
+++ frontend/src/components/SearchForm.js
@@ -1,10 +1,10 @@
 import React, { useCallback, useState, useContext } from 'react';
 import {
-  Box, Grommet, Keyboard, TextInput, Text, Form, FormField, Button, MaskedInput, ResponsiveContext,
+  Box, Grommet, TextInput, Text, Form, FormField, Button, ResponsiveContext,
 } from 'grommet';
 import _ from 'lodash';
 
-import { Search } from 'grommet-icons';
+import { Search, StatusWarning } from 'grommet-icons';
 
 import { useNavigate } from 'react-router-dom';
 import locationAutocomplete from '../utils/autocomplete';
@@ -12,8 +12,9 @@ import locationAutocomplete from '../utils/autocomplete';
 const customTheme = {
   textInput: {
     extend: () => `
+    border: none;
     &:focus {
-      box-shadow: 0 0 1px 0 #999;
+      box-shadow: none;
     }
   `,
   },
@@ -30,25 +31,29 @@ const customTheme = {
       `,
   },
   formField: {
-    border: 'none',
+    error: {
+      icon: <StatusWarning color="status-critical" />,
+      container: {
+        align: 'center',
+        margin: {
+          left: '10px',
+        },
+      },
+      background: {
+      },
+    },
+    border: {
+      width: '0px',
+      side: 'none',
+      radius: '4px',
+    },
   },
 };
 
-function CoordinateFormField({ name, id }) {
+function CoordinateFormField({ validate, name, id }) {
   return (
-    <FormField name={name} htmlFor={id} direction="row" align="center" margin={{ bottom: '0px' }}>
+    <FormField name={name} htmlFor={id} align="center" margin={{ bottom: '0px' }} validate={validate}>
       <Box width={{ max: '80px' }}>
-        {/* <MaskedInput
-          mask={[
-            {
-              regexp: /^-?[0-9]{1,2}\.?(\.[0-9]{1,3})?$/,
-            },
-          ]}
-          id={id}
-          name={name}
-          placeholder="00.000"
-          focusIndicator={false}
-        /> */}
         <TextInput name={name} id={id} placeholder={id} />
       </Box>
     </FormField>
@@ -87,48 +92,81 @@ function SearchForm() {
     navigate(`./forecast/lat:${encodeURIComponent(value.latitude)},lon:${encodeURIComponent(value.longtitude)}`);
   };
 
+  const validateSearchForm = (validationResults) => {
+    console.log(validationResults);
+  };
+
+  const validateSelectField = (fieldValue, formValue) => {
+    if (!fieldValue && (!formValue.longtitude && !formValue.latitude)) {
+      return <Box alignSelf="start"><Text>select location</Text></Box>;
+    }
+    return undefined;
+  };
+
+  const validateLatitude = (fieldValue, formValue) => {
+    if (!fieldValue) {
+      if (formValue.longtitude) {
+        return <Box><Text>enter latitude</Text></Box>;
+      }
+    } else if (fieldValue >= 90 || fieldValue <= -90 || !fieldValue.match(/^-?[0-9]{1,2}\.?(\.[0-9]{1,3})?$/)) {
+      return <Box><Text>incorrect value</Text></Box>;
+    }
+    return undefined;
+  };
+
+  const validateLongtitude = (fieldValue, formValue) => {
+    if (!fieldValue) {
+      if (formValue.latitude) {
+        return <Box><Text>enter longtitude</Text></Box>;
+      }
+    } else if (fieldValue >= 180 || fieldValue <= -180 || !fieldValue.match(/^-?[0-9]{1,3}\.?(\.[0-9]{1,3})?$/)) {
+      return <Box><Text>incorrect value</Text></Box>;
+    }
+    return undefined;
+  };
+
   const screenSize = useContext(ResponsiveContext);
 
   return (
     <Grommet theme={customTheme}>
-      <Box direction={screenSize === 'small' ? 'column' : 'row'} align="center" wrap gap="10px">
-        {screenSize !== 'small' && <Box><Text>Select location</Text></Box>}
-        <Box direction="row">
-          <Keyboard onEnter={() => navigate(`./search/${encodeURIComponent(query)}`)}>
-            <Box
-              direction="row"
-              align="center"
-              ref={boxRef}
-              wrap
-            >
-              <Box>
-                <TextInput
-                  type="search"
-                  icon={<Search />}
-                  dropTarget={boxRef.current}
-                  onChange={(event) => { setQuery(event.target.value); loadSuggestions(event.target.value); }}
-                  value={query}
-                  onSuggestionSelect={(event) => selectPlace(event.suggestion)}
-                  placeholder={screenSize === 'small' ? 'Select location...' : 'Enter...'}
-                  suggestions={suggestions}
-                />
-              </Box>
-            </Box>
-          </Keyboard>
-        </Box>
-        <Box pad={{ vertical: 'medium' }}>
-          <Text>or enter coordinates:</Text>
-        </Box>
-        <Form onSubmit={searchByCoordinates}>
-          <Box direction={screenSize === 'small' ? 'column' : 'row'} wrap gap="20px">
-            <Box direction="row" gap="10px">
-              <CoordinateFormField name="latitude" id="lat" label="Lat" />
-              <CoordinateFormField name="longtitude" id="lon" label="Lon" />
-            </Box>
-            <Button margin={screenSize === 'small' && { vertical: 'medium' }} type="submit" label="Search" />
+      <Form onSubmit={searchByCoordinates} onValidate={validateSearchForm} validate="blur" direction="row">
+        <Box direction="row" align="center" gap="10px">
+          {screenSize !== 'small' && (
+          <Box direction="row" align="center" gap="10px">
+            <Search />
+            <Text>Select location</Text>
           </Box>
-        </Form>
-      </Box>
+          )}
+          <Box
+            align="center"
+            ref={boxRef}
+            wrap
+          >
+            <FormField name="select" htmlFor="select" validate={validateSelectField} margin={{ bottom: '0px' }}>
+              <TextInput
+                id="select"
+                name="select"
+                type="search"
+                icon={screenSize === 'small' && <Search />}
+                dropTarget={boxRef.current}
+                onChange={(event) => { setQuery(event.target.value); loadSuggestions(event.target.value); }}
+                value={query}
+                onSuggestionSelect={(event) => selectPlace(event.suggestion)}
+                placeholder={screenSize === 'small' ? 'Select location...' : 'e. g. London...'}
+                suggestions={suggestions}
+              />
+            </FormField>
+          </Box>
+          <Box>
+            <Text>or enter coordinates:</Text>
+          </Box>
+          <Box direction="row" gap="10px">
+            <CoordinateFormField validate={validateLatitude} name="latitude" id="lat" label="Lat" />
+            <CoordinateFormField validate={validateLongtitude} name="longtitude" id="lon" label="Lon" />
+          </Box>
+          <Button margin={screenSize === 'small' && { vertical: 'medium' }} type="submit" label="Search" />
+        </Box>
+      </Form>
     </Grommet>
   );
 }
