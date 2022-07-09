fdc460aca1e60217084b83d22cf84c4bb25e9c01
Yulia <schonebud@gmail.com>
2022-07-09 16:45:13 +0300

    Change API for autocomplete of location and sending location coordinates

--- frontend/src/utils/autocomplete.js
+++ frontend/src/utils/autocomplete.js
@@ -1,11 +1,20 @@
 import axios from 'axios';
 
 const locationAutocomplete = async (query) => {
-  const results = await axios.get(`https://autocomplete.search.hereapi.com/v1/autocomplete?limit=20&lang=en-US&q=${query}&apiKey=4SIwA2_PMQuFpVFrLJENdTsg6fZjhkgtHjvTu-xa6fc`);
-  const locations = results.data.items.filter((place) => (
-    place.resultType === 'locality' || (place.resultType === 'administrativeArea' && (place.administrativeAreaType !== 'country' || place.administrativeAreaType !== 'county'))
-  )).map((item) => { console.log(item); return item.address.label; });
+  const results = await axios.get(`https://api.geoapify.com/v1/geocode/autocomplete?text=${query}&lang=en&limit=10&type=city&format=json&apiKey=69386fcf9d784091a2b4aa3b33b67113`);
+  //const results = await axios.get(`https://autocomplete.search.hereapi.com/v1/autocomplete?limit=20&lang=en-US&q=${query}&apiKey=4SIwA2_PMQuFpVFrLJENdTsg6fZjhkgtHjvTu-xa6fc`);
+
+  const locations = results.data.results.map((item) => {
+    const formattedItem = {
+      name: `${item.city}, ${item.district}, ${item.state}, ${item.country}`,
+      coordinates: { lat: item.lat, lon: item.lon },
+    };
+    console.log(formattedItem);
+    return formattedItem;
+  });
   return locations;
 };
 
 export default locationAutocomplete;
+
+//69386fcf9d784091a2b4aa3b33b67113
